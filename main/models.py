9a7b84cb44f04cfba1b6f7568425c817ff578852
Korb1t <oles.hobor@gmail.com>
2019-06-19 15:07:10 +0300

    proj model

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class Slider(models.Model):
                               null=True,
                               blank=True,
                               help_text='Зображення буде відображатись на слайдері головної сторінки')
-    title = models.CharField('Title',
+    title = models.CharField('Заголовок',
                              max_length=225,
                              help_text='Тайл для бокса на слайдері')
     description = models.TextField('Опис',
@@ -20,3 +20,22 @@ class Slider(models.Model):
     class Meta:
         verbose_name = 'Фотографія Слайдеру'
         verbose_name_plural = 'Фотографії Сладеру'
+
+
+class Project(models.Model):
+    image = models.ImageField('Зображення',
+                              upload_to='img',
+                              null=True,
+                              blank=True)
+    title = models.CharField('Заголовок',
+                             max_length=225,
+                             help_text='Заголовок проекта')
+    description = models.TextField('Опис',
+                                   help_text='Опис проекта')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекти'
