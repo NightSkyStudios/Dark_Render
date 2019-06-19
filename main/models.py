2ad45542834f5548f58afd4596747a63338deddc
Korb1t <oles.hobor@gmail.com>
2019-06-19 14:30:26 +0300

    merge

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Slider(models.Model):
+    image = models.ImageField('Зображення',
+                              upload_to='img',
+                              null=True,
+                              blank=True,
+                              help_text='Зображення буде відображатись на слайдері головної сторінки')
+    title = models.CharField('Title',
+                             max_length=225,
+                             help_text='Тайл для бокса на слайдері')
+    description = models.TextField('Опис',
+                                   max_length=125,
+                                   help_text='Текст для бокса в коробці')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фотографія Слайдеру'
+        verbose_name_plural = 'Фотографії Сладеру'
