f526cc9ac2aed93b9d72370b0f214836b6839873
Korb1t <oles.hobor@gmail.com>
2019-06-19 14:40:00 +0300

    slider

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    slider = Slider.objects.all()
+
+    ctx = {'slider': slider}
+
+    return render(request, 'index.html', ctx)
 
 
 def about(request):
